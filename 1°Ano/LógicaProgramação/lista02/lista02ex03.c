67dfa33bd0b45e160f7eccf3381fdfd1913d1513
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2026-03-06 21:52:31 -0300

    Update lista02ex03.c

--- 1°Ano/LógicaProgramação/lista02/lista02ex03.c
+++ 1°Ano/LógicaProgramação/lista02/lista02ex03.c
@@ -1,10 +1,16 @@
-
-
-#include <stdio.h>
-
-int main(int argc, char **argv)
+int main()
 {
-	
+	float val1, val2, val3, val4, med;
+	printf("informe o primeiro valor:");
+	scanf("%f", &val1);
+	printf("informe o segundo valor:");
+	scanf("%f", &val2);
+	printf("informe o terceiro valor:");
+	scanf("%f", &val3);
+	printf("informe o quarto valor:");
+	scanf("%f", &val4);
+	med = (val1+val2+val3+val4)/4;
+	printf("a media dos valores(%2f,%2f,%2f,%2f) é de :%2f",
+	val1,val2,val3,val4,med);	
 	return 0;
 }
-
